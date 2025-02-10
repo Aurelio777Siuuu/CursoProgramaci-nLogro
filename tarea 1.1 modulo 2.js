747c712393b7fa9f0086ec562df04bcf3d137430
Aurelio777Siuuu <reyaltabara2.0@gmail.com>
2025-02-10 14:54:27 -0400

    Add files via upload

---
+++ tarea 1.1 modulo 2.js
@@ -0,0 +1,8 @@
+let and = (5>3) && (2<4)
+console.log("su resutado and es " + and)
+
+let or = (5>3) || (2>4)
+console.log("su resultado or es " + or)
+
+let negacion = ! (5<3)
+console.log("su resultado del símbolo de negación es " + negacion)
