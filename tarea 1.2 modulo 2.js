12e7be00da12e5e93aa852f863785c5f143d8ee2
Aurelio777Siuuu <reyaltabara2.0@gmail.com>
2025-02-10 16:14:23 -0400

    Add files via upload

---
+++ tarea 1.2 modulo 2.js
@@ -0,0 +1,22 @@
+let numero = 90;
+if (numero >= 0) {
+    console.log("Su número es positivo "+ numero);}
+    else if (numero < 0) {
+    console.log("Su número es negativo");}
+    else {
+        console.log("Su número es cero")
+    }
+
+console.log("=================")
+
+for (let i = 1; i <= 10; i++) {
+    console.log("sus numeros con for: " + i);
+}
+
+console.log("=================")
+
+let i = 1;
+do {
+    console.log("sus numeros con while: " + i);
+    i++;
+} while (i<11)
